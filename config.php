b6f1e8f60a95979ac511e5cb80a53ecd6878b5dd
AntonioEduardo-Dev <antonio.eduardo.dev@gmail.com>
2024-08-15 13:05:55 -0300

    feat: add config

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+define('BASE_URL', 'http://localhost/');
+
+/* Database Credentials */
+
+define('DB_HOST', 'db');
+define('DB_PORT', '3306');
+define('DB_USER', 'root_docker');
+define('DB_PASS', '1234');
+define('DB_NAME', 'phpDB');
